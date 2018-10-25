ebdd763ba77659bb59054f954acee34d6ce75fb5
egoist301 <filippovich.z@mail.ru>
2018-10-26 00:23:09 +0300

    Update Calculator.java

--- lab6/src/by/bntu/fitr/povt/filippovich/triangle/model/Calculator.java
+++ lab6/src/by/bntu/fitr/povt/filippovich/triangle/model/Calculator.java
@@ -1,17 +1,17 @@
 package by.bntu.fitr.povt.filippovich.triangle.model;
 
 public class Calculator {
-    public static double CalculateArea(double a, double b){
+    public double CalculateArea(double a, double b){
         double area = (a * b) / 2;
         return area;
     }
 
-    public static double CalculateHypotenuse(double a, double b){
+    public double CalculateHypotenuse(double a, double b){
         double c = Math.sqrt(Math.pow(a,2) + Math.pow(b,2));
         return c;
     }
 
-    public static  double CalculatePerimeter(double a, double b){
+    public double CalculatePerimeter(double a, double b){
         double perimeter = a + b + CalculateHypotenuse(a, b);
         return perimeter;
     }
