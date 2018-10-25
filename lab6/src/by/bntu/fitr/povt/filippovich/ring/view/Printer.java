aa00c19e8e948c1e6ca378592f33ed0ee37ae510
egoist301 <filippovich.z@mail.ru>
2018-10-26 00:24:01 +0300

    Update Printer.java

--- lab6/src/by/bntu/fitr/povt/filippovich/ring/view/Printer.java
+++ lab6/src/by/bntu/fitr/povt/filippovich/ring/view/Printer.java
@@ -1,7 +1,7 @@
 package by.bntu.fitr.povt.filippovich.ring.view;
 
 public class Printer {
-    public static void print(String msg) {
+    public void print(String msg) {
         System.out.println(msg);
     }
 }
