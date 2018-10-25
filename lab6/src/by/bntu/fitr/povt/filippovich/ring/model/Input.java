bb08039c46cd8a65f196d89300e4790eb1edc07b
egoist301 <filippovich.z@mail.ru>
2018-10-26 00:21:39 +0300

    Update Input.java

--- lab6/src/by/bntu/fitr/povt/filippovich/ring/model/Input.java
+++ lab6/src/by/bntu/fitr/povt/filippovich/ring/model/Input.java
@@ -3,7 +3,7 @@ package by.bntu.fitr.povt.filippovich.ring.model;
 import java.util.Scanner;
 
 public class Input {
-    public static double UserInput(String msg){
+    public double UserInput(String msg){
         Scanner sc = new Scanner(System.in);
         System.out.println(msg);
         double value = sc.nextDouble();
