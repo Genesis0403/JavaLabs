658201db5fd9cdae4affc2f84bbafffa2b7b3d92
egoist301 <filippovich.z@mail.ru>
2018-10-26 00:22:23 +0300

    Update Controller.java

--- lab6/src/by/bntu/fitr/povt/filippovich/ring/controller/Controller.java
+++ lab6/src/by/bntu/fitr/povt/filippovich/ring/controller/Controller.java
@@ -5,7 +5,7 @@ import by.bntu.fitr.povt.filippovich.ring.model.Calculator;
 import by.bntu.fitr.povt.filippovich.ring.model.Input;
 
 public class Controller {
-    public static void Execute(){
+    public void Execute(){
         double r1 = Input.UserInput("Input R1: ");
         double r2 = Input.UserInput("Input R2: ");
         double area = Calculator.RingArea(r1, r2);
