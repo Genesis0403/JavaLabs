3a107ee86beef857bceab910f009c0f307ef8260
egoist301 <filippovich.z@mail.ru>
2018-10-26 00:22:01 +0300

    Update Calculator.java

--- lab6/src/by/bntu/fitr/povt/filippovich/ring/model/Calculator.java
+++ lab6/src/by/bntu/fitr/povt/filippovich/ring/model/Calculator.java
@@ -1,11 +1,11 @@
 package by.bntu.fitr.povt.filippovich.ring.model;
 
 public class Calculator {
-    private static double CircleArea(double r){
+    private double CircleArea(double r){
         double area = Math.PI * Math.pow(r,2);
         return area;
     }
-    public static double RingArea(double r1, double r2){
+    public double RingArea(double r1, double r2){
         double areaRing = CircleArea(r1) - CircleArea(r2);
         return areaRing;
     }
