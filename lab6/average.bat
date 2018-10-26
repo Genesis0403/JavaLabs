2de0e2c1b604763fb0349e3f0cd9f74f54626eff
Vlad Korotkevich <vlad.korotkevich2@gmail.com>
2018-10-26 12:55:43 +0300

    average and distance .bat applied.

---
+++ lab6/average.bat
@@ -0,0 +1,4 @@
+md target
+javac -d target -sourcepath src/ src/by/bntu/fitr/povt/korotkevich/average/Main.java
+java -cp target by.bntu.fitr.povt.korotkevich.average.Main
+pause
---
+++ lab6/distance.bat
@@ -0,0 +1,4 @@
+md target
+javac -d target -sourcepath src/ src/by/bntu/fitr/povt/korotkevich/distance/Main.java
+java -cp target by.bntu.fitr.povt.korotkevich.distance.Main
+pause
